e9b5d29887f8cd769c2c82fbfb7ddea0cdf27ae8
Adharsh <adharshkrish@outlook.com>
2020-02-29 08:05:45 +0530

    active element concept

--- skeleton/js/menu.js
+++ skeleton/js/menu.js
@@ -1,18 +1,16 @@
-// let c = document.getElementById('menu-col-2').getElementsByTagName('ul');
-// for(var i = 0; i < c.length; i++) {
-//     console.log(c[i]);
-// }
-
-function menu(item)
+function menu(elt,item)
 {
     document.getElementById('menu-col-3').classList.replace('col-lg-6','col-lg-3');
     document.getElementById('menu-col-3').classList.replace('col-lg-2','col-lg-3');
     document.getElementById('menu-col-4').classList.replace('col-lg-4','col-lg-3');
     document.getElementById('menu-col-3').style.borderRight='none';
-    // for(let i=0;i<6;i++){
-    //     document.getElementById('menu-col-2').classList.replace('col-lg-3','col-lg-2');
-    // }
 
+    var m2 = document.getElementsByClassName("menu2");
+    for (i = 0; i < m2.length; i++)
+        m2[i].classList.remove("active-m2");
+
+    elt.classList.add('active-m2');
+    
     if(item==='gallery'){
         document.getElementById('menu-col-3').innerHTML='<img src="gallery-1.jpg" alt="gallery-1">';
         document.getElementById('menu-col-4').innerHTML='<img src="gallery-2.jpg" alt="gallery-2">';
@@ -21,7 +19,7 @@ function menu(item)
         document.getElementById('menu-col-3').classList.replace('col-lg-3','col-lg-2');
         document.getElementById('menu-col-4').classList.replace('col-lg-3','col-lg-4');
         document.getElementById('menu-col-3').style.borderRight='1px solid #707070';
-        document.getElementById('menu-col-3').innerHTML='<ul><li>Campus&nbsp;Life</li><li onmouseover="menu1(1)">Committees<i class="material-icons" style="font-size:15px;margin-top:12px;float:right">navigate_next</i></li><li onmouseover="menu1(2)">Cells<i class="material-icons" style="font-size:15px;margin-top:12px;float:right">navigate_next</i></li><li onmouseover="menu1(3)">Clubs<i class="material-icons" style="font-size:15px;margin-top:12px;float:right">navigate_next</i></li><li onmouseover="menu1(4)">Chapters<i class="material-icons" style="font-size:15px;margin-top:12px;float:right">navigate_next</i></li><li>Facilities</li></ul>';
+        document.getElementById('menu-col-3').innerHTML='<ul><li class="menu3">Campus&nbsp;Life</li><li class="menu3" onmouseover="menu1(this,1)">Committees<i class="material-icons" style="font-size:15px;margin-top:12px;float:right">navigate_next</i></li><li class="menu3" onmouseover="menu1(this,2)">Cells<i class="material-icons" style="font-size:15px;margin-top:12px;float:right">navigate_next</i></li><li class="menu3" onmouseover="menu1(this,3)">Clubs<i class="material-icons" style="font-size:15px;margin-top:12px;float:right">navigate_next</i></li><li class="menu3" onmouseover="menu1(this,4)">Chapters<i class="material-icons" style="font-size:15px;margin-top:12px;float:right">navigate_next</i></li><li class="menu3">Facilities</li></ul>';
         document.getElementById('menu-col-4').innerHTML='';
     }
     else if(item==='departments'){
@@ -34,52 +32,21 @@ function menu(item)
         document.getElementById('menu-col-4').innerHTML='<img src="college-img.jpg" alt="college-img">';
     }
 }
-function menu1(i){
-    if(i==1){
+function menu1(elt,index)
+{
+    var m2 = document.getElementsByClassName("menu3");
+    for (var i = 0; i < m2.length; i++)
+        m2[i].classList.remove("active-m2");
+
+    elt.classList.add('active-m2');
+
+    if(index==1){
         document.getElementById('menu-col-4').innerHTML='<ul><li>Alumni Association</li><li>Examination Wing</li><li>Internal Complaints Committee</li><li>National Cadet Corps</li><li>National Service Scheme</li><li>Students’ Council</li><li>Students’ Disciplinary Committee</li></ul>';
-    }else if(i==2){
+    }else if(index==2){
         document.getElementById('menu-col-4').innerHTML='<ul><li>Center for Continuing Education</li><li>Counselling and Career Guidance Cell</li><li>Grievance Redressal Cell</li><li>Innovation and Entrepreneurship Development Cell</li><li>Internal Quality Assurance Cell</li><li>IP Rights and Patenting Cell</li><li>SC/ST Cell</li><li>Womens’ Cell</li></ul>';
-    }else if(i==3){
+    }else if(index==3){
         document.getElementById('menu-col-4').innerHTML='<ul><li>Cultural Club</li><li>Design Club</li><li>Health, Hygiene and Environmental Club</li><li>Literary Club</li><li>Multimedia Club</li><li>Photography Club</li><li>Rotaract Club</li><li>Tamil Mandram</li></ul>';
-    }else if(i==4){
+    }else if(index==4){
         document.getElementById('menu-col-4').innerHTML='<ul style="font-size:18px"><li>National Programme on Technology Enhanced Learning</li><li>Quality Enhancement in Engineering Education</li><li>Swayam</li></ul>';
     }
 }
-/*
-Cells-
-Center for Continuing Education (CCE)
-Counselling and Career Guidance Cell (CCGC)
-Grievance Redressal Cell
-Innovation and Entrepreneurship Development Cell (IEDC)
-Internal Quality Assurance Cell (IQAC)
-IP Rights and Patenting Cell
-SC/ST Cell
-Women’s Cell
-Clubs-
-Cultural Club
-Design Club
-Health, Hygiene and Environmental Club (H2EC)
-Literary Club
-Multimedia Club
-Photography Club
-Rotaract Club
-Tamil Literary Club 
-Committees-
-Alumni Association
-Examination Wing
-Internal Complaints Committee (Complaints on Sexual Harassment of Women at Workplace)
-National Cadet Corps (NCC)
-National Service Scheme (NSS)
-Students’ Council
-Students’ Disciplinary Committee
-Chapters-
-National Programme on Technology Enhanced Learning (NPTEL)
-Quality Enhancement in Engineering Education (QEEE)
-Swayam
-Academics-
-Deans
-
-
-<ul><li>National Programme on Technology Enhanced Learning</li><li>Quality Enhancement in Engineering Education</li><li>Swayam</li></ul>
-
-*/
